a316cbfb51bca305f780de75fbf5cfa70ba986e3
harshrajbatham <harshrajbatham8435@gmail.com>
2023-06-19 14:44:57 +0530

    Diff VAR LET CONST

---
+++ Variables/VarLetConst.js
@@ -0,0 +1,11 @@
+//the Var, Let, Const diffrance
+var a='name'
+var a='name1'
+console.log(a) // in var assign value multiple times but it prints only latest value.
+
+let b = 'naam'
+    b = 'naam1'
+console.log(b) // in let declair only one time but assign multple values.
+
+const c ='namak'
+ console.log(c) // one n only no  any ther like var & let.
