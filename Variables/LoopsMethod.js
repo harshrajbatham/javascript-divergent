ce292a34eb39c74fba5b0930c2d1978d9f1534b8
harshrajbatham <harshrajbatham8435@gmail.com>
2023-06-19 16:46:33 +0530

    loop methods

---
+++ Variables/LoopsMethod.js
@@ -0,0 +1,12 @@
+// loop no.1
+let items= ["Paper","Pen","Pencils","Sherpner"];
+for(let i= 0;i<items.length;i++){
+    console.log(items)
+}           
+// loop no.2 While loops
+
+let j=1;
+while(j<=5){
+    console.log("while loop",j);
+    j++;
+}
