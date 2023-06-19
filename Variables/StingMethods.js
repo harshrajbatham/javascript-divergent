6ce2f76368512f8a1c2ef4022ab5809b3c5d19c6
harshrajbatham <harshrajbatham8435@gmail.com>
2023-06-19 14:08:43 +0530

    StringMethod with Comments

--- Variables/StingMethods.js
+++ Variables/StingMethods.js
@@ -1,12 +1,11 @@
 let name = "Harshraj Batham";
 
- console.log(name.toUpperCase());
+ console.log(name.toUpperCase()); // for uppercase
 
-console.log(name.startsWith("Harsh"));
+console.log(name.startsWith("Harsh")); // for check string in start with !
 
-console.log(name.lastIndexOf("Batham"));
+console.log(name.lastIndexOf("Batham")); // string indexing from last
 
-console.log (name.split(''))
+console.log (name.split('')) // spit the word in each space
 
-console.log(name ,typeof name) //
-
+console.log(name ,typeof name) // check the data type
