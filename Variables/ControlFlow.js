285fdbb3f4fd9ae00290522767c037e1af55e0ab
harshrajbatham <harshrajbatham8435@gmail.com>
2023-06-19 17:57:34 +0530

    control flow statement

---
+++ Variables/ControlFlow.js
@@ -0,0 +1,13 @@
+// nested if else control flow statement
+
+let number = 20;
+let numCorrect = 2020;
+if (number >= 18) {
+  if (numCorrect) {
+    console.log("corecct ans");
+  } else {
+    console.log("Not Correct and");
+  }
+} else {
+  console.log("The end");
+}
---
+++ Variables/conditionMethods.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  // another one
+  let i = 4;
+
+  for (let j = 0; i < 10; i++) {
+    if (j === 1 || i === 6) {
+      continue;
+    } else {
+      console.log(i, j);
+  
+      if (i === 7) {
+        break;
+      }
+    }
+  }
+  
