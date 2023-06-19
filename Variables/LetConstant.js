f84234e3a37e4f4761b7f5b1b6f79d7c5d2b85fb
harshrajbatham <harshrajbatham8435@gmail.com>
2023-06-19 13:50:24 +0530

    study variables and string methods

---
+++ Variables/LetConstant.js
@@ -0,0 +1,2 @@
+let a="HARSH"
+    console.log(a)
---
+++ Variables/StingMethods.js
@@ -0,0 +1,12 @@
+let name = "Harshraj Batham";
+
+ console.log(name.toUpperCase());
+
+console.log(name.startsWith("Harsh"));
+
+console.log(name.lastIndexOf("Batham"));
+
+console.log (name.split(''))
+
+console.log(name ,typeof name)
+
