4c3aa9503e9ad3dbf3dd138379cccc3757443f7a
harshrajbatham <harshrajbatham8435@gmail.com>
2023-06-21 17:03:39 +0530

    Var,Let Questions

--- Question/21-06-2023/VarLetConstQuestions.js
+++ Question/21-06-2023/VarLetConstQuestions.js
@@ -1,8 +1,50 @@
 //Q1: What are let, const, and var used for in JavaScript?
+// they all are data type declearation for a variable!
 //code:
+//var a=1;
+//let b=2;
+//const ab=11;
+//console.log(a,b,ab)
+//--------------------------------------------------------------------------------------------------------//
+//Q2: What is the difference between let, const, and var?
+// let can use with in function and out of the function.
+//var can use within a function and scope!
+//cnost can use once only  or Globally!
+//-------------------------------------------------------------------------------------------------------//
+//Q3: When should I use let?
+//let allows us to declare variables that are limited to the scope of a block statement, or expression on which it is used
+//code:
+//let x = 0;
+//y = 1;
+//console.log(sumOf(x,y));
+//let y;
+//function sumOf(a, b){
+ //    return a + b; }
+//----------------------------------------------------------------------------------------------------//
+//Q4: When should I use const?
+//const prevents the variable to be assigned to another value.
+//---------------------------------------------------------------------------------------------------//
+//Q5: When should I use var?
+//it is used to declares within function-scoped
+//--------------------------------------------------------------------------------------------------//
+//Q8: Are let, const, and var hoisted in JavaScript?
+// let and const variables are hoisted, only they are hoisted without a default initialization
+//code
+//function greetings() {
+  //  console.log("Heelo",name);
+  //}
+  
+  //let name = "Aakash";
+  
+  //greetings();
 
+//-------------------------------------------------------------------------------------------------//
+//Q9. What is Hoisting in javascript?
+//code
+//Hoisting in JavaScript is a behavior in which a function or a variable can be used before declaration. 
+// it helps compiler to execute from top
 
-
+//-------------------------------------------------xx-----------------------------------------------//
 
 
 
